3ff50664d4b72852190f0cb4938e0d164d39cdb0
rry <r.yamakuchi@gmail.com>
2019-10-31 03:58:14 +0900

    add: node-sass sass-loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,25 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        octrouble-web
-      </h1>
-      <h2 class="subtitle">
-        My legendary Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <el-container>
+    <el-header>Octrouble Web</el-header>
+    <el-main>GitHub リポジトリを検索して Issues を見ることができます。</el-main>
+  </el-container>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<style lang="scss" scoped>
+.el-container {
+  background-color: #f4f5f9;
 
-export default {
-  components: {
-    Logo
+  .el-header {
+    background-color: #252c41;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
   }
-}
-</script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .el-main {
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
 }
 </style>
